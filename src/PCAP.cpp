37e95d454a2467de15f56fad9ad82e9109b77728
deltax <deltax.fluxion@gmail.com>
2018-02-27 15:02:40 +0100

    Remove package length check
    - Remove package lenght check since it brick the the pcap file.

--- src/PCAP.cpp
+++ src/PCAP.cpp
@@ -77,9 +77,9 @@ void PCAP::newPacketSerial(uint32_t ts_sec, uint32_t ts_usec, uint32_t len, uint
   uint32_t orig_len = len;
   uint32_t incl_len = len;
   
-#if defined(ESP32)
+
   if(incl_len > snaplen) incl_len = snaplen; /* safty check that the packet isn't too big (I ran into problems with the ESP8266 here) */
-#endif
+
   serialwrite_32(ts_sec);
   serialwrite_32(ts_usec);
   serialwrite_32(incl_len);
